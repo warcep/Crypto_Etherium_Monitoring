0ecb254987f260f217da448c5ba988e073a21b63
warcep <36923406+warcep@users.noreply.github.com>
2020-08-20 21:30:29 +0200

    4

--- main.py
+++ main.py
@@ -12,20 +12,16 @@ def ether():
         txn_hash_table = soup.table
         table_rows  = txn_hash_table.find_all('tr')
 
+        webHeader =['Txn Hash','Block','Date Time (UTC)','DIFF','FROM','SELL','TO','VALUE','[TXN Fee]']
         allData=[]
         for tr in table_rows:
             td = tr.find_all('td')
             if len(td)!=0:
                 allData.append([i.text for i in td])
-
-        webHeader =[]
-        for th in table_rows:
-            th = th.find_all('th')
-            if len(th)!=0:
-                webHeader.append([i.text for i in td])
-
         df = pd.DataFrame(allData)
-        df.to_csv('filename.csv', index=False,header=webHeader)
+        df.to_csv('filename.csv', index=False,header=webHeader,sep='\t')
+
+
 
 
     except Exception as Err:
